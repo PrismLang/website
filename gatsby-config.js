53a872b9c98de12c044b5c2e54e38c0fed52842f
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-21 17:20:51 +0530

    gatsby: add config file

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+    siteMetadata: {
+        title: "Prism Programming Language",
+        description: "Prism is a simple programming language aimed at beginners!",
+        siteUrl: "https://prism.traction.com",
+    },
+    plugins: [
+        "gatsby-plugin-emotion",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-typescript",
+    ],
+};
