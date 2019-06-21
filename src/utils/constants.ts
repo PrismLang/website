8cf06cf86b85c6200ae88034ae6dff0e040362b1
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-21 17:21:32 +0530

    utils: add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+const COLORS = {
+    TONE1: "#9f82fe",
+    TONE2: "#906ffe",
+    TONE3: "#815cfe",
+    TONE4: "#7349fe",
+
+    BORDER_LIGHT: "#8369d6",
+    BORDER_DARK: "#5d37d8",
+
+    WHITE: "#ffffff",
+    LIGHT: "#f0f0f0",
+    LIGHT_GREY: "#bbbbbb",
+    GREY: "grey",
+    DARK: "#111111",
+    BLACK: "#000000",
+};
+
+export {
+    COLORS,
+};
