8d5e8107d643a9bd162d0de8e06930af08dab0ca
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-22 10:32:42 +0530

    layouts(default): integrate Helment & other improvements

--- src/layouts/default/index.tsx
+++ src/layouts/default/index.tsx
@@ -4,18 +4,28 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Main from "../../components/Main";
 import BackToTop from "../../components/BackToTop";
+import Helmet from "../../components/Helmet";
 
 import "../base.css";
 
-const DefaultLayout: React.FunctionComponent = ({ children }) => (
-  <div>
-    <Header />
-    <Main>
-      { children }
-    </Main>
-    <Footer />
-    <BackToTop />
-  </div>
+interface ILayoutProps {
+    title?: string;
+    description?: string;
+}
+
+const DefaultLayout: React.FunctionComponent<ILayoutProps> = (props) => (
+    <div>
+        <Helmet
+            title={ props.title }
+            description={ props.description }
+        />
+        <Header />
+        <Main>
+            { props.children }
+        </Main>
+        <Footer />
+        <BackToTop />
+    </div>
 );
 
 export default DefaultLayout;
