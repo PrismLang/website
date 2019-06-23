06d4a0eea4b6363d2bd79bb40fae7f19b505713c
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-23 07:38:56 +0530

    components(Footer): add social links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,52 @@
 import React from "react";
+import { Link } from "gatsby";
+import { FaGithub, FaReddit, FaTwitter } from "react-icons/fa";
 
 import { COLORS } from "../../utils/constants";
+
+interface ISocialLinkProps {
+    link: {
+        title: string;
+        href: string;
+    }
+}
+const SocialLink: React.FunctionComponent<ISocialLinkProps> = (props) => (
+    <a href={ props.link.href } title={ props.link.title } target="_blank" css={{
+        color: "inherit",
+        fontSize: "1.5em",
+        margin: 5,
+    }}>
+        { props.children }
+    </a>
+);
+
+const SocialLinks: React.FunctionComponent = () => (
+    <div>
+        <SocialLink link={{ title: "GitHub", href: "https://github.com/PrismLang" }}>
+            <FaGithub />
+        </SocialLink>
+        <SocialLink link={{ title: "Twitter", href: "https://twitter.com/PrismLang" }}>
+            <FaTwitter />
+        </SocialLink>
+        <SocialLink link={{ title: "Reddit", href: "https://reddit.com/r/PrismLang" }}>
+            <FaReddit />
+        </SocialLink>
+    </div>
+);
 
 const FooterLinks: React.FunctionComponent = () => (
     <div css={{
         padding: "2rem 8rem",
         display: "flex",
         justifyContent: "space-between",
+        alignItems: "center",
+        flexWrap: "wrap-reverse",
         color: COLORS.GREY,
         backgroundColor: COLORS.DARK,
         fontSize: ".8em",
     }}>
-        <div>
-            Prism Programming Language
-        </div>
+        <div></div>
+        <SocialLinks />
     </div>
 );
 
